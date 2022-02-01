bae29edf192a17436d9f0362243fbe545e56571c
ludovic-juniper <ludovic@juniper.net>
2022-02-01 11:06:36 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,8 +14,29 @@ provider "ibm" {
 ##############################################################################
 # Read/validate Region
 ##############################################################################
+
+#data "ibm_is_region" "region" {
+#  name = var.region
+#}
+
+variable "generation" {
+  default     = 2
+  description = "The VPC Generation to target. Valid values are 2 or 1."
+}
+
+provider "ibm" {
+  /* Uncomment ibmcloud_api_key while testing from CLI */
+  #ibmcloud_api_key      = "${var.api_key}"
+  generation            = "${var.generation}" 
+  region                = "${var.region}"
+  ibmcloud_timeout      = 300
+}
+
+##############################################################################
+# Read/validate Region
+##############################################################################
 data "ibm_is_region" "region" {
-  name = var.region
+  name = "${var.region}"
 }
 
 ##############################################################################
